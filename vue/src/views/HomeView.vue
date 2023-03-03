c3de274a443d6ba86c5faba966e801c739607b12
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-03-03 13:14:13 -0500

    3/3/23 lesson

--- vue/src/views/HomeView.vue
+++ vue/src/views/HomeView.vue
@@ -1,3 +1,24 @@
-<script setup></script>
-
-<template></template>
+<template>
+  <h1 v-if="selected === 'A'">A</h1>
+  <h1 v-else-if="selected === 'B'">B</h1>
+  <h1 v-else-if="selected === 'C'">C</h1>
+  <h1 v-else>Choose A, B, or C</h1>
+  <select v-model="selected">
+    <option value="A">A</option>
+    <option value="B">B</option>
+    <option value="C">C</option>
+  </select>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      graduated: false,
+      student: "Yifan",
+      animals: ["pig", "cow", "ostrich", "Leon"],
+      text: "",
+      selected: "",
+    };
+  },
+};
+</script>
