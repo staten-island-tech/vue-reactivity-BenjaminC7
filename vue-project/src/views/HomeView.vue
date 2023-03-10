acadd296010e74532e194e266acd37c592747bd3
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-03-10 13:13:29 -0500

    I don't understand vue tbh

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -1,9 +1,24 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <h1 v-if="graduated">{{ student }}</h1>
+    <h1 v-else>Else text</h1>
+  </div>
 </template>
+
+<script>
+export default {
+  name: "Home",
+  components: {},
+  data() {
+    return {
+      student: "Student",
+      graduated: true,
+    };
+  },
+};
+</script>
+<style scoped>
+h1 {
+  color: blue;
+}
+</style>
