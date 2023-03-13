506b412f68890570c608a4193e326034b16b6409
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-03-13 13:14:42 -0400

    l5 1:32

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -2,6 +2,10 @@
   <div>
     <h1 v-if="graduated">{{ student }}</h1>
     <h1 v-else>Else text</h1>
+    <button @click="authState" v-if="loggedIn">Log Out</button>
+    <button @click="authState" v-else>Login</button>
+    <input type="text" placeholder="edit me" v-model="message" />
+    <p>{{ message }}</p>
   </div>
 </template>
 
@@ -11,9 +15,20 @@ export default {
   components: {},
   data() {
     return {
+      loggedIn: true,
       student: "Student",
       graduated: true,
+      message: "",
     };
+  },
+  methods: {
+    authState: function () {
+      if (this.loggedIn === false) {
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false;
+      }
+    },
   },
 };
 </script>
