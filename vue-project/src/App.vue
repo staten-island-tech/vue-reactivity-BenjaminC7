e97d4b4abd55b252e1f223c58eae1860c028f411
BenjaminC7 <113201246+BenjaminC7@users.noreply.github.com>
2023-03-21 12:42:20 -0400

    oh no this was bad

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -1,81 +1,50 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
-</script>
-
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <h1 v-if="graduated">{{ student }}</h1>
+    <h1 v-else>Else text</h1>
+    <button @click="authState" v-if="loggedIn">Log Out</button>
+    <button @click="authState" v-else>Login</button>
+    <input type="text" placeholder="edit me" v-model="message" />
+    <p>{{ message }}</p>
+  </div>
+  <h2>{{ now }}</h2>
+  <ButtonComp>This text can change</ButtonComp>
 </template>
 
+<script>
+import ButtonComp from "./components/ButtonComp.vue";
+export default {
+  name: "Home",
+  components: {
+    ButtonComp,
+  },
+  data() {
+    return {
+      loggedIn: true,
+      student: "Student",
+      graduated: true,
+      message: "",
+    };
+  },
+  methods: {
+    authState: function () {
+      if (this.loggedIn === false) {
+        this.loggedIn = true;
+      } else {
+        this.loggedIn = false;
+      }
+    },
+  },
+  computed: {
+    now: function () {
+      const time = new Date();
+      return `${time.getHours()}:${time.getMinutes()}`;
+    },
+  },
+};
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+h1 {
+  color: blue;
 }
 </style>
